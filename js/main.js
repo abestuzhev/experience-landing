ba46a133ada324a76796b6987d4d79b6c90057e3
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-02-07 18:03:23 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-    
+
+
+  // $('section').addClass("hidden").viewportChecker({
+  //   classToAdd: 'visible animated fadeIn',
+  //   offset: 100
+  // });
+
+  $(".footer-btn-up").on("click", function(e){
+    e.preventDefault();
+    // $("body").scrollTo(0);
+    $('body,html').animate({
+        scrollTop: 0
+      }, 700
+    );
+  });
 });
