d9314a81d5dd41de5590ec339e4bc63d59095894
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-02-08 11:39:10 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
 
+//плавный появление
+  $('section').addClass("hidden").viewportChecker({
+    classToAdd: 'visible animated fadeIn',
+    offset: 100
+  });
 
-  // $('section').addClass("hidden").viewportChecker({
-  //   classToAdd: 'visible animated fadeIn',
-  //   offset: 100
-  // });
+//плавный якорь
+  $(".c-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
 
   function setEqualHeight(columns) {
     var tallestcolumn = 0;
@@ -19,7 +27,7 @@ $(document).ready(function () {
     columns.height(tallestcolumn);
   }
   setEqualHeight($(".employment-item"));
-  
+
 
   $(".footer-btn-up").on("click", function(e){
     e.preventDefault();
@@ -28,4 +36,30 @@ $(document).ready(function () {
       }, 700
     );
   });
+
+  $('.reviews-student .slider').slick({
+      autoplay: false,
+      autoplaySpeed: 5000,
+      dots: false,
+      arrows: true,
+      infinite: true,
+      speed: 500,
+      adaptiveHeight: false,
+      slidesToShow: 3,
+      slidesToScroll: 1
+  });
+
+    $('.reviews-employers .slider').slick({
+        autoplay: false,
+        autoplaySpeed: 5000,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        adaptiveHeight: false,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+
+    $('.phone-mask').mask('+7(000)000-00-00');
 });
